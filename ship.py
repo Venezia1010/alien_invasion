8f355d68e1cceac447b994bdfcd8dbc986ed88db
venezia1010 <malakhov.valentin@inbox.ru>
2024-01-21 18:41:37 +0300

    для маленького упражнения было добавлено стартовое положение корабля в центре экрана

--- ship.py
+++ ship.py
@@ -27,3 +27,18 @@ class Ship:
     def blitme(self):
         """Рисует корабль в текущей позиции"""
         self.screen.blit(self.image, self.rect)
+
+
+class Ship_2:
+    """Класс добавляет корабль нацентр экрана"""
+    def __init__(self, screen):
+        self.screen = screen
+
+        self.image = pygame.image.load('images/sokol_1.png')
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.centery = self.screen_rect.centery
+
+    def blitme(self):
+        self.screen.blit(self.image, self.rect)
