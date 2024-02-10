d96245229e32d7f797ec92cbe8e638f83ef061d0
venezia1010 <malakhov.valentin@inbox.ru>
2024-02-10 19:05:39 +0300

    добавлены события для кнопки play

--- game_functions.py
+++ game_functions.py
@@ -31,7 +31,7 @@ def check_keyup_events(event, ship):
         ship.moving_bottom = False
 
 
-def check_events(ai_settings, screen, ship, bullets):
+def check_events(ai_settings, screen, stats, play_button, ship, bullets):
     """Обрабатывает нажатия клавишь и события мыши"""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -40,6 +40,15 @@ def check_events(ai_settings, screen, ship, bullets):
             check_keydown_events(event, ai_settings, screen, ship, bullets)
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
+        elif event.type == pygame.MOUSEBUTTONDOWN:            
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            check_play_button(stats, play_button, mouse_x, mouse_y)
+
+
+def check_play_button(stats, play_button, mouse_x, mouse_y):
+    """Запускает новую игру на нажатии кнопки Play"""
+    if play_button.rect.collidepoint(mouse_x, mouse_y):
+        stats.game_active = True
 
 
 def update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button):
