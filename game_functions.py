bf6e6bc3073165ad36cbf46f8594919e952e0eb9
venezia1010 <malakhov.valentin@inbox.ru>
2024-02-18 16:01:10 +0300

    Иправлен баг с кнопкой Play

--- game_functions.py
+++ game_functions.py
@@ -47,7 +47,8 @@ def check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets)
 
 def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bullets, mouse_x, mouse_y):
     """Запускает новую игру на нажатии кнопки Play"""
-    if play_button.rect.collidepoint(mouse_x, mouse_y):
+    button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
+    if button_clicked and not stats.game_active:
 
         # Сброс игровой статистики
         stats.reset_stats()
